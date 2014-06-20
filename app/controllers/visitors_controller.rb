c5a3a13ecb6ed2217c46fd5fd8628392076c742b
Jeff Milewski <jeffmilewski@gmail.com>
2014-06-20 14:53:24 -0500

    add from to home page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,25 @@
 class VisitorsController < ApplicationController
 
   def new
-  	flash[:notice] = 'Welcome!'
-  	flash.now[:alert] = 'Access not allowed.'
+  	#flash[:notice] = 'Welcome!'
+  	#flash.now[:alert] = 'Access not allowed.'
+  	@visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      flash.now[:notice] = "Chose #{@visitor.favorite}."
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:favorite, :comment)
   end
 
 end
